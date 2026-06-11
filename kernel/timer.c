e2eeee3c761027b42eec1d1c6e110eac036c2dff
lutsc <170535336+lutsc@users.noreply.github.com>
2026-06-10 23:03:53 -0300

    Adicionado .align no trap_entry.S, adicionado outro método de timer_next();

--- kernel/timer.c
+++ kernel/timer.c
@@ -3,7 +3,7 @@
 static uint64_t tick_interval = 100000;
 
 // Especificação do SBI
-#define SBI_EID 0x00 // Extension ID
+#define SBI_EID 0 // Extension ID
 
 static inline void sbi_set_timer(uint64_t time){
   register uint64_t a0 asm("a0") = time;
@@ -19,12 +19,23 @@ static inline void sbi_set_timer(uint64_t time){
 void timer_next(void){
 	uint64_t now;
 
-  // pseudo-instrução que lê o valor de tempo do CSR (Control and Status Register) 
-  asm volatile("rdtime %0" : "=r"(now) : : "memory"); 
-  
+  // lê o valor de tempo do CSR (Control and Status Register) 
+  asm volatile ("rdtime %0" : "=r"(now));
+
   // tempo atual + intervalo
   sbi_set_timer(now + tick_interval);
 }
+
+// // método sem SBI, talvez seja necessário botar extensão SSTC no makefile
+// void timer_next(void) {
+//   uint64_t now;
+
+//   // lê o valor de tempo do CSR (Control and Status Register)
+//   asm volatile("rdtime %0" : "=r"(now));
+  
+//   // escreve no CSR stimecmp (0x14D)
+//   asm volatile("csrw 0x14D, %0" :: "r"(now + tick_interval));
+// }
 
 void timer_init(uint64_t interval){
   if (interval != 0)
@@ -34,9 +45,9 @@ void timer_init(uint64_t interval){
 
   // ativa o 5° bit do reg. sie, habilitando interrupção do timer 
   // basicamente se o timer disparar, o modo supervisor interrompe o código
-  asm volatile("csrs sie, %0" :: "r"(1 << 5));
+  asm volatile("csrs sie, %0" :: "r"(1UL << 5));
   
   // ativa o 1° bit do reg. sstatus, habilitando interrupções em geral
   // tem que ativar pra qualquer interrupção acontecer
-  asm volatile("csrs sstatus, %0" :: "r"(1 << 1));
+  asm volatile("csrs sstatus, %0" :: "r"(1UL << 1));
 }
