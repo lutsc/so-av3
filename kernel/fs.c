0fc907ec248a26ed497e757d118860c5a4a3b087
lutsc <170535336+lutsc@users.noreply.github.com>
2026-06-29 20:32:30 -0300

    Implementado fs_init e cluster_alloc, adicionado layout para 2048 blocos, comentários em partes implementadas

--- kernel/fs.c
+++ kernel/fs.c
@@ -3,28 +3,37 @@
 #include "uart.h"
 #include <string.h>
 
-// Constantes
+// Constantes FAT
 #define SIMPLEFAT_MAGIC 0x53464154 // Flag de SimpleFAT
 #define FAT_FREE 0x0000 // Flag cluster livre
 #define FAT_EOF 0xFFFF // Flag fim de cluster
 
-#define NUM_CLUSTERS 32 // Qtd. de clusters
-#define CLUSTER_SIZE 512 // Qtd. bytes de cada cluster
+// Tamanho e número diversos
+#define DISK_SIZE (1024*1024) // Tamanho de 1MB
+#define BLOCK_SIZE 512 // Qtd. bytes de cada bloco
+#define CLUSTER_SIZE 512 // Qtd. bytes de cada cluster, igual a bloco para encaixar
+#define NUM_CLUSTERS 2048 // Qtd. de clusters
 
 #define MAX_FILENAME 32 // Qtd. char para nome do arquivo
-#define MAX_DIR_ENTRIES 32 // Qtd. de diretórios
+#define MAX_DIR_ENTRIES 64 // Qtd. de diretórios
+
+// Layout dos blocos (Baseado em 2048 blocos)
+#define SUPERBLOCK_START_BLOCK 0 // Bloco que inicia o superbloco [0]
+#define FAT_START_BLOCK 1 // Bloco que inicia a seção de FAT [1 a 8]
+#define DIR_START_BLOCK 9 // Bloco que inicia a seção de diretórios [9 a 10]
+#define DATA_START_BLOCK 11 // Bloco que inicia a seção de dados [11 a 2047]
 
 // Estrutura do superbloco
 typedef struct{
-  uint32_t magic;
-  uint32_t total_blocks;
-  uint32_t total_clusters;
+  uint32_t magic; // Número mágico
+  uint32_t total_blocks; // Total de blocos
+  uint32_t total_clusters; // Total de clusters
   uint32_t cluster_size; // Qtd. bytes da cluster
 } superblock_t;
 
 // Estrutura do diretório
 typedef struct{
-  char name[MAX_FILENAME]; 
+  char name[MAX_FILENAME]; // Nome do arquivo
   uint32_t size; // Qtd. bytes do arquivo
   uint16_t first_cluster; // "Ponteiro" do 1° cluster
 } dir_entry_t;
@@ -35,29 +44,38 @@ static uint16_t fat[NUM_CLUSTERS];
 static dir_entry_t root[MAX_DIR_ENTRIES];
 
 int fs_init(void){
-	// Iniciando superbloco
+	// Inicializando superbloco
 	sb.magic = SIMPLEFAT_MAGIC;
-	// sb.total_blocks = // ?
+	sb.total_blocks = DISK_SIZE / BLOCK_SIZE;
 	sb.total_clusters = NUM_CLUSTERS;
 	sb.cluster_size = CLUSTER_SIZE;
 	
-	/*
-	* TODO:
-	* Inicializar FAT.
-	*/
-	/*
-	* TODO:
-	* Inicializar diretrio.
-	*/
+	// Alocando espaço pro superbloco
+	uint8_t buf[BLOCK_SIZE];
+	memset(buf, 0, BLOCK_SIZE);
+	memcpy(buf, &sb, sizeof(sb));
+	block_write(SUPERBLOCK_START_BLOCK, buf);
+
+	// Inicializando FAT
+	memset(fat, 0, sizeof(fat));
+
+	// Inicializando diretório
+	memset(root, 0, sizeof(root));
+	
+	uart_print("SimpleFAT inicializado.\n");
+	
 	return 0;
 }
 
 int cluster_alloc(void){
-	/*
-	* TODO:
-	* Procurar FAT_FREE.
-	*/
-	return -1;
+	for (int i = 1; i < NUM_CLUSTERS; i++) {
+		if (fat[i] == FAT_FREE) {
+			fat[i] = FAT_EOF;
+			return i;
+		}
+	} 
+	uart_print("Sem clusters livres.\n");
+  return -1;
 }
 
 int fs_create(const char *name){
