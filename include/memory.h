76fab5813f574be021d68a7cbaa969d0f1335b10
lutsc <170535336+lutsc@users.noreply.github.com>
2026-06-29 16:00:59 -0300

    Estrutura futura para manusear memória

--- include/memory.h
+++ include/memory.h
@@ -3,7 +3,13 @@
 #include <stdint.h>
 
 void memory_init(void);
+
 void *kmalloc(uint64_t size);
+
+// void kfree(void *ptr);
+
+// uint64_t memory_free(void);
+
 uint64_t memory_used(void);
 
 #endif
