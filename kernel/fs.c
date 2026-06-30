cdb17bdc32608d69a06c0b50b54c1b2f5b89aeb7
lutsc <170535336+lutsc@users.noreply.github.com>
2026-06-30 19:31:30 -0300

    Adicionado estrutura de descritor de arquivo, adicionado função helper de checar nomes iguais, adicionado fs_create

--- kernel/fs.c
+++ kernel/fs.c
@@ -12,7 +12,9 @@
 #define NUM_CLUSTERS 2048 // Qtd. de clusters
 
 #define MAX_FILENAME 32 // Qtd. char para nome do arquivo
-#define MAX_DIR_ENTRIES 64 // Qtd. de diretórios
+#define MAX_DIR_ENTRIES 64 // Qtd. máxima de diretórios na root
+
+#define MAX_OPEN_FILES 8 // Qtd. máxima de arquivos em um diretório
 
 // Layout dos blocos (Baseado em 2048 blocos)
 #define SUPERBLOCK_START_BLOCK 0 // Bloco que inicia o superbloco [0]
@@ -33,12 +35,21 @@ typedef struct{
   char name[MAX_FILENAME]; // Nome do arquivo
   uint32_t size; // Qtd. bytes do arquivo
   uint16_t first_cluster; // "Ponteiro" do 1° cluster
+	uint8_t used; // Ocupado = 1, Livre = 0
 } dir_entry_t;
 
+// Estrutura do descritor de arquivo
+typedef struct {
+	uint32_t pos; // Posição em bytes de leitura/escrita
+	uint32_t dir_index; // Índice que fica no diretório root[]
+	uint8_t used; // Ocupado = 1, Livre = 0
+} file_desc_t;
+
 // Variáveis globais
-static superblock_t sb;
-static uint16_t fat[NUM_CLUSTERS];
-static dir_entry_t root[MAX_DIR_ENTRIES];
+static superblock_t sb; // Superbloco
+static uint16_t fat[NUM_CLUSTERS]; // Clusters
+static dir_entry_t root[MAX_DIR_ENTRIES]; // Diretórios
+static file_desc_t fdt[MAX_OPEN_FILES]; // Descritores de arquivos
 
 // Funções helper
 void mem_set(void *pointer, int value, uint32_t num) {
@@ -91,19 +102,58 @@ int cluster_alloc(void){
   return -1;
 }
 
+// Percorre nome do arquivo (array de char) e verifica se são iguais (Igual = 1, Diferente = 0)
+int check_available_filename(const char *file1, const char *file2) {
+	for (int i = 0; i < MAX_FILENAME; i++) {
+		if (file1[i] != file2[i])
+			return 0; 
+		if (file1[i] == '\0')
+			return 1;
+	}
+	return 1; 
+}
+
 int fs_create(const char *name){
-	/*
-	* TODO:
-	* Procurar entrada livre.
-	*/
-	/*
-	* TODO:
-	* Alocar cluster inicial.
-	*/
-	/*
-	* TODO:
-	* Atualiza Diretrio
-	*/
+	// Procura diretório livre para arquivo
+	int free_slot = -1;
+	for (int i = 0; i < MAX_DIR_ENTRIES; i++) {
+		if (root[i].used) {
+			// Checa se há um arquivo do mesmo nome
+			if (check_available_filename(root[i].name, name)) {
+				uart_print("Arquivo com mesmo nome já existe.\n");
+				return -1;
+			}
+		} else if (free_slot == -1) {
+			free_slot = i; // Guarda índice se slot está livre
+		}
+	}
+
+	if (free_slot == -1) {
+		uart_print("Diretório cheio.\n");
+		return -1;
+	}
+
+	// Procura cluster livre
+	int free_cluster = cluster_alloc();
+	if(free_cluster == -1){
+		return -1;
+	}
+
+	// Atualizar diretório
+	mem_set(&root[free_slot], 0, sizeof(dir_entry_t));
+
+	// Passa nome do arquivo para o arquivo do slot
+	for (int i = 0; (i < (MAX_FILENAME-1)) && (name[i] != '\0'); i++)
+		root[free_slot].name[i] = name[i];
+
+	// Inicializa arquivo com 0 bytes de dados, cluster inicial e marca ocupado
+	root[free_slot].size = 0;
+	root[free_slot].first_cluster = (uint16_t)free_cluster;
+	root[free_slot].used = 1;
+
+	uart_print("Arquivo criado: ");
+	uart_print(name);
+	uart_print("\n");
 	return 0;
 }
 
