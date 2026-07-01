8473e0416294dd3148a3b0d8f01f07d6c26f467b
lutsc <170535336+lutsc@users.noreply.github.com>
2026-07-01 00:01:22 -0300

    Testando funções implementadas no main, funcionando

--- kernel/main.c
+++ kernel/main.c
@@ -4,6 +4,7 @@
 #include "memory.h"
 #include "timer.h"
 #include "trap.h"
+#include "fs.h"
 
 extern void uart_print(const char*);
 extern void trap_entry(void);
@@ -19,17 +20,33 @@ void task2(void){
 }
 
 void kernel_main(){
-	memory_init();
+	// memory_init();
 
 	uart_print("\n=== Kernel ===\n");
 
-	xTaskCreate(task1, 2048, 1);
-	xTaskCreate(task2, 2048, 1);
+	// xTaskCreate(task1, 2048, 1);
+	// xTaskCreate(task2, 2048, 1);
 
-	asm volatile("csrw stvec, %0" :: "r"(trap_entry));
+	// asm volatile("csrw stvec, %0" :: "r"(trap_entry));
 
-	timer_init(100000);
+	// timer_init(100000);
 
-	scheduler_start();
-	while (1);
+	// scheduler_start();
+	// while (1);
+
+	fs_init();
+	fs_create("notas.txt");
+	fs_create("notas.txt");
+
+	fs_delete("notas.txt");
+
+	fs_create("notas.txt");
+
+	// int fd = fs_open("notas.txt");
+	// fs_write(fd, "Sistemas Operacionais", 22);
+
+	// char buffer[64];
+	// fs_read(fd, buffer, 22);
+
+	// fs_close(fd);
 }
