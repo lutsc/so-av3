12c45ae380325515213840caf0102c7799f8edde
lutsc <170535336+lutsc@users.noreply.github.com>
2026-07-01 19:56:04 -0300

    Adicionado testes para verificar funcionamento das funções

--- kernel/main.c
+++ kernel/main.c
@@ -5,6 +5,7 @@
 #include "timer.h"
 #include "trap.h"
 #include "fs.h"
+#include "string.h"
 
 extern void uart_print(const char*);
 extern void trap_entry(void);
@@ -20,33 +21,64 @@ void task2(void){
 }
 
 void kernel_main(){
+	uart_print("\n=== Kernel ===\n\n");
+	
 	// memory_init();
-
-	uart_print("\n=== Kernel ===\n");
-
 	// xTaskCreate(task1, 2048, 1);
 	// xTaskCreate(task2, 2048, 1);
 
 	// asm volatile("csrw stvec, %0" :: "r"(trap_entry));
-
 	// timer_init(100000);
-
 	// scheduler_start();
 	// while (1);
 
 	fs_init();
+	uart_print("\n");
+
+	// Não permite arquivos iguais
 	fs_create("notas.txt");
 	fs_create("notas.txt");
+	uart_print("\n");
+
+	// Consegue depois de deletado
+	fs_delete("notas.txt");
+	fs_create("notas.txt");
+	uart_print("\n");
+
+	// Escrita
+	int fd = fs_open("notas.txt");
+	fs_write(fd, "Sistemas Operacionais\n", 23);
+	fs_close(fd);
+
+	// Leitura
+	fd = fs_open("notas.txt");
+	char buffer[64];
+	fs_read(fd, buffer, 23);
+	uart_print(buffer);
+	fs_close(fd);
+	uart_print("\n");
 
 	fs_delete("notas.txt");
+	uart_print("\n");
 
-	fs_create("notas.txt");
+	// Testes
+	fs_create("teste1.txt");
+	fd = fs_open("teste1.txt");
+	// fs_write(fd, "teste1\n", 1024*1024);
+	fs_write(fd, "teste1\n", 1024*1018);
+	fs_close(fd);
 
-	// int fd = fs_open("notas.txt");
-	// fs_write(fd, "Sistemas Operacionais", 22);
+	fs_delete("teste1.txt");
+	uart_print("\n");
 
-	// char buffer[64];
-	// fs_read(fd, buffer, 22);
+	fs_create("teste2.txt");
+	fd = fs_open("teste2.txt");
+	fs_write(fd, "teste2\n", 64);
+	fs_close(fd);
 
-	// fs_close(fd);
+	fs_create("teste3.txt");
+	fd = fs_open("teste3.txt");
+	fs_write(fd, "teste3\n", 64);
+	fs_close(fd);
+
 }
